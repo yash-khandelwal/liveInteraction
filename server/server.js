148d0c8efaab284f30707efa06bd4e4ee31b3c58
yash-khandelwal <kritagya.0398@gmail.com>
2021-01-02 12:14:59 +0530

    structured

---
+++ server/server.js
@@ -0,0 +1,70 @@
+const http = require("http");
+const express = require("express");
+const socketio = require('socket.io');
+const cors = require("cors");
+
+const Users = require('./utils/users');
+let users = new Users();
+const router = require('./utils/router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server,{
+    path: '/socket.io',
+    maxHttpBufferSize: 10000,
+    cookie: false
+});
+
+app.use(cors());
+app.use(router);
+
+io.on('connect', (socket) => {
+    
+    console.log("connected");
+    // when new user enters in to the channel
+    socket.on('join', ({userName, displayName, channel}, callback) => {
+        const {error, user} = users.addUser({id: socket.id, userName, displayName, channel});
+        if(error){
+            return callback(error);
+        }
+        socket.join(user.channel);
+        socket.emit('message', {
+            user: 'admin', 
+            text: `${user.displayName} joined!`
+        });
+        io.to(user.channel).emit('channelData', { 
+            channel: user.channel, 
+            users: users.getUsersInChannel(user.channel) 
+        });
+        console.log("joined");
+        callback();
+    });
+    
+    // when user send message in the channel
+    socket.on('sendMessage', (message, callback) => {
+        const user = users.getUser(socket.id);
+        io.to(user.channel).emit('message', {user: user.displayName, text: message});
+        callback();
+    });
+    
+    // when user go offline from the channel
+    socket.on('disconnect', () => {
+        const user = users.getUser(socket.id);
+        if(user){
+            io.to(user.channel).emit('message', {
+                user: 'admin', 
+                text: `${user.displayName} has left`
+            });
+            io.to(user.channel).emit('channelData', {
+                channel: user.channel,
+                users: users.getUsersInChannel(user.channel)
+            });
+        }
+    });
+})
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port: ${PORT}`);
+})
---
+++ server/utils/users.js
@@ -0,0 +1,37 @@
+class Users{
+    constructor(){
+        this.users = [];
+    }
+    addUser = ({id, userName, displayName, channel}) => {
+        // if user already exists
+        const userExist = this.users.find((user) => {
+            return user.userName === userName && user.channel === channel
+        });
+        if(userExist){
+            return { errors: [ { msg: "User already Exists!"} ] };
+        }
+        const newUser = {
+            id,
+            userName,
+            displayName,
+            channel
+        }
+        this.users.push(newUser);
+        return newUser;
+    }
+    removeUser = (id) => {
+        this.users = this.users.filter((user) => {
+            return user.id !== id;
+        })
+        return this.users;
+    }
+    getUser = (id) => {
+        return this.users.find((user) => {
+            return user.id === id;
+        })
+    }
+    getUsersInChannel = (channel) => {
+        return this.users.filter((user) => user.channel === room);
+    }
+}
+module.exports = Users;
